f64ce87e406093e0cb62a7f27b0cd7c374ba2d46
stasuwe <stasuwe@gmail.com>
2015-09-03 16:12:20 +0300

    upload module finish with some issues. Start tooltip module

--- app/js/modules.js
+++ app/js/modules.js
@@ -2,84 +2,210 @@
 
 var upload = (function(){
 
+	var 
+		_isBasicImageLarge = false;
+
 	function initialization(){
 		_setupEventListeners();
-		_imageUpload('#basicImage', _basicImageDoneCallback);
-		_imageUpload('#waterMark', _waterMarkDoneCallback);
-		//_formUpload();
 	}
 
 	function _setupEventListeners(){
-		$('#main-form').on('submit', _formSubmitHandler);
+		$('#basicImage').on('change', _basicImageChangeHandler);
+
+		$('#waterMark').on('change', _waterMarkChangeHandler);
+
 	}
 
-	function _formSubmitHandler(e){
-		e.preventDefault();
-		
+	//basicImage onchage handler
+	function _basicImageChangeHandler(e){
 		var 
-			data = $(this).serializeArray();
-		console.log(data);
-		// var
-		// 	$this = $(this);
-
-		// $this.fileupload('send', {})
+			$this = $(this),
+			img = $this[0].files[0];
+		
+		//if img not undefind read it from local machine
+		if(img){
+			_readFile.call(this, img, _basicImageLoadCallback);
+		}
+		
 	}
 
-	function _formUpload(){
-		var
-			form = $('#main-form');
-
-		form.fileupload({
-			dataType: 'json',
-			url: 'php/main.php',
-			replaceFileInput: false,
-			autoUpload: false,
-			done: function(e,data){
-				console.log('form sbmt');
-			}
-		});
-	}
-
-	function _basicImageDoneCallback(e,data){
+	//callback function for basikImage loading
+	function _basicImageLoadCallback(e){
 		var
 			$this = $(this),
+			img = $this[0].files[0],
 			imgContainer = $('.main-area'),
 			imgMarkup = '<img src="" class="basicImage__img" />',
-			waterMark = $this.closest('.upload').find('#waterMark');
+			waterMark = $this.closest('.upload').find('#waterMark'),
+			basicImage = $('.basicImage__img');
 
-		$this.siblings('.imitation-upload').find('input').val(data.files[0].name);
+		if(basicImage.length){
+			basicImage.remove();
+			_isBasicImageLarge = false;
+		} 
+		basicImage = imgContainer.append(imgMarkup).find('.basicImage__img').attr('src', e.target.result);
 		waterMark.prop('disabled',false);
-		imgContainer.append(imgMarkup).find('.basicImage__img').attr('src', data.result.img);
-	}
 
-	function _waterMarkDoneCallback(e,data){
-		var
-			$this = $(this),
-			imgContainer = $('.main-area'),
-			disabledArea = $('.disabled-area'),
-			imgMarkup = '<img src="" class="waterMark__img" />';
+		$this.siblings('.imitation-upload').find('input').val(img.name);
 
-		$this.siblings('.imitation-upload').find('input').val(data.files[0].name);
-		imgContainer.append(imgMarkup).find('.waterMark__img').attr('src', data.result.img);
-		_controlsInit();
+		basicImage.on('load', function(e){
+			var 
+				$this = $(this),
+				imgContainer = $('.main-area'),
+				imgNaturalWidth = $this.prop('naturalWidth'),
+				imgNaturalHeight = $this.prop('naturalHeight'),
+				imgWidth = $this.width(),
+				imgHeight = $this.height(),
+				imgContainerWidth = imgContainer.width(),
+				imgContainerHeight = imgContainer.height();
 
-	}
+			if(imgNaturalWidth > imgContainerWidth || imgNaturalHeight > imgContainerHeight){
 
-	function _imageUpload(input_id, callback){
-		var
-			el = $(input_id);
+				_isBasicImageLarge = true;
 
-		el.fileupload({
-			dataType: 'json',
-			url: 'php/upload.php',
-			replaceFileInput: false,
-			done: callback
+				if(imgNaturalWidth > imgNaturalHeight){
+
+					$this.css('top', Math.round((imgContainerHeight - imgHeight)/2) + 'px' );
+
+				} else {
+
+					$this.css('left', Math.round((imgContainerWidth - imgWidth)/2) + 'px' );	
+				}
+
+			} else {
+
+				$this.css({
+					'top': Math.round((imgContainerHeight - imgHeight)/2) + 'px',
+					'left': Math.round((imgContainerWidth - imgWidth)/2) + 'px' 	
+				});
+				
+			}
+			$this.css('opacity', 1);
+
 		});
 	}
 
-	function _controlsInit(){
-
+	//waterMark onchage handler
+	function _waterMarkChangeHandler(e){
+		var 
+			$this = $(this),
+			img = $this[0].files[0];
+		//if img not undefind read it from local machine
+		if(img){
+			_readFile.call(this, img, _waterMarkLoadCallback);
+		}
+		
 	}
+
+	//callback function for waterMark loading
+	function _waterMarkLoadCallback(e){
+		var
+			$this = $(this),
+			img = $this[0].files[0],
+			imgContainer = $('.main-area'),
+			disabledArea = $('.disabled-area'),
+			imgMarkup = '<img src="" class="waterMark__img" />',
+			waterMark = $('.waterMark__img');
+
+		if(waterMark.length){
+			waterMark.remove();
+		} 
+		waterMark = imgContainer.append(imgMarkup).find('.waterMark__img').attr('src', e.target.result);
+		disabledArea.css('display', 'none');
+	
+		$this.siblings('.imitation-upload').find('input').val(img.name);
+
+
+
+		waterMark.on('load', function(e){
+			var 
+				$this = $(this),
+				imgContainer = $('.main-area'),
+				basicImage = $('.basicImage__img'),
+				basicImageNaturalWidth = basicImage.prop('naturalWidth'),
+				basicImageNaturalHeight = basicImage.prop('naturalHeight'),
+				basicImagePosition = basicImage.position(),  
+				imgNaturalWidth = $this.prop('naturalWidth'),
+				imgNaturalHeight = $this.prop('naturalHeight'),
+				imgWidth = $this.width(),
+				imgHeight = $this.height(),
+				imgContainerWidth = imgContainer.width(),
+				imgContainerHeight = imgContainer.height(),
+				relImgWidth = 0,
+				relImgHeight = 0,
+				relIndex = imgNaturalWidth / imgNaturalHeight;
+
+			if(imgNaturalWidth > basicImageNaturalWidth || imgNaturalHeight > basicImageNaturalHeight){
+				$this.remove();
+				console.log('tooltip: image is to big');
+			} else {
+
+				if( _isBasicImageLarge ){
+
+					if( basicImageNaturalWidth > basicImageNaturalHeight ){
+
+						relImgWidth = (imgNaturalWidth * imgContainerWidth) / basicImageNaturalWidth;
+						relImgHeight = relImgWidth / relIndex;
+
+					} else if ( basicImageNaturalHeight > basicImageNaturalWidth ){
+
+						relImgHeight = (imgNaturalHeight * imgContainerHeight) / basicImageNaturalHeight;
+						relImgWidth = relImgHeight * relIndex;
+					}
+
+					$this.css({
+						'width': Math.round(relImgWidth) + 'px',
+						'height': Math.round(relImgHeight) + 'px'
+					});
+
+				} 
+
+				 $this.css({
+				 	'top': basicImagePosition.top + 'px',
+					'left': basicImagePosition.left + 'px',
+					'opacity': 1 	
+				 });
+				
+			}		
+
+		});
+		
+	}
+
+	//upload images from local storage
+	function _readFile(file, callback){
+		var
+			reader = new FileReader();
+
+		//manipulation image after load
+		$(reader).on('load', $.proxy(callback,this));
+
+
+		$(reader).on('loadstart', function(e){
+			if(e.originalEvent.total > 5000000){
+				this.abort();
+			}
+		});
+
+		$(reader).on('abort', $.proxy(function(){
+			console.log('tooltip: image size is to big');
+		},this));
+
+		//show progressbar
+		$(reader).on('progress', function(e){
+			var
+				oe = e.originalEvent,
+				progress = 0;
+
+			if(oe.lengthComputable){
+				progress = parseInt( ((oe.loaded / oe.total) * 100), 10);
+				console.log('progress: '+progress);
+			}
+		});
+
+		reader.readAsDataURL(file);
+	}
+
 
 	return {
 		init: initialization
